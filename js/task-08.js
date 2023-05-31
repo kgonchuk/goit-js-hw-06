074ee0e9acf4ecaebf7a52b321322940011f28ee
kgonchuk <123867028+kgonchuk@users.noreply.github.com>
2023-05-31 12:40:15 +0300

    added task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const formEl = document.querySelector('.login-form');
+console.dir(formEl);
+formEl.addEventListener('submit', onSubmit);
+function onSubmit(evt) {
+    evt.preventDefault();
+
+    if ( formEl.value === '') {
+        alert ('Порожні поля!') 
+    
+    } else {
+        const {email, password } = evt.currentTarget.elements;
+console.log(email.value);
+    console.log(password.value);
+    
+    const inpEl = {
+        name: email.value,
+        name: password.value
+        
+}
+}
+
+
+
+}
 
